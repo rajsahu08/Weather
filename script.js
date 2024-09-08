8caf71b9ed977eb1286d52dba45558ae0d50e0d2
rajsahu08 <rajsahu0702@gmail.com>
2024-09-09 00:55:24 +0530

    Completed

--- script.js
+++ script.js
@@ -10,7 +10,18 @@ const pressure = document.querySelector(".pressure");
 const humidity = document.querySelector(".humidity");
 const visibility = document.querySelector(".visibility");
 const clouds = document.querySelector(".clouds");
-
+const date1 = document.querySelector(".date1");
+const temp1 = document.querySelector(".temp1");
+const text1 = document.querySelector(".text1");
+const icon1 = document.querySelector(".icon1");
+const date2 = document.querySelector(".date2");
+const temp2 = document.querySelector(".temp2");
+const text2 = document.querySelector(".text2");
+const icon2 = document.querySelector(".icon2");
+const date3 = document.querySelector(".date3");
+const temp3 = document.querySelector(".temp3");
+const text3 = document.querySelector(".text3");
+const icon3 = document.querySelector(".icon3");
 
 // Set an interval to update the background image based on the current hour every 60 seconds
 setInterval(() => {
@@ -99,7 +110,21 @@ let getWeatherForecast= async(url)=>{
   try {
     const response = await fetch(url, options);
     const result = await response.json();
-    console.log(result);
+    date1.textContent = await result.forecast.forecastday[0].date;
+    temp1.textContent = await result.forecast.forecastday[0].day.avgtemp_c + "° C";
+    text1.textContent = await result.forecast.forecastday[0].day.condition.text;
+    icon1.src = await result.forecast.forecastday[0].day.condition.icon;
+
+    date2.textContent = await result.forecast.forecastday[1].date;
+    temp2.textContent = await result.forecast.forecastday[1].day.avgtemp_c + "° C";
+    text2.textContent = await result.forecast.forecastday[1].day.condition.text;
+    icon2.src = await result.forecast.forecastday[1].day.condition.icon;
+
+    date3.textContent = await result.forecast.forecastday[2].date;
+    temp3.textContent = await result.forecast.forecastday[2].day.avgtemp_c + "° C";
+    text3.textContent = await result.forecast.forecastday[2].day.condition.text;
+    icon3.src = await result.forecast.forecastday[2].day.condition.icon;
+    console.log(result.forecast);
   } catch (error) {
     console.error(error);
   }
