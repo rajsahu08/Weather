4d369dabf830dd8eef58d24f041c3855b00a6d96
rajsahu08 <rajsahu0702@gmail.com>
2024-12-05 22:53:11 +0530

    Changed API

--- script.js
+++ script.js
@@ -23,13 +23,14 @@ const temp3 = document.querySelector(".temp3");
 const text3 = document.querySelector(".text3");
 const icon3 = document.querySelector(".icon3");
 
-// Set an interval to update the background image based on the current hour every 60 seconds
+//We have Set an interval to update the background image based on the current hour every 60 seconds
 setInterval(() => {
   let currTime = new Date();
   let currHours = currTime.getHours();
   changeBackground(currHours);
 }, 60000);
-// Function to change the background image based on the current hour
+
+// this function is used to change the background image based on the current hour
 function changeBackground(currHours) {
   console.log(currHours);
   let body = document.querySelector("body");
@@ -56,8 +57,8 @@ if (navigator.geolocation) {
     lat = position.coords.latitude;
     lon = position.coords.longitude;
     console.log("Latitude: " + lat + ", Longitude: " + lon);
-    let url = `https://weatherapi-com.p.rapidapi.com/current.json?q=${lat}%2C${lon}`;
-    getWeatherInfo(url);
+    let url = `https://open-weather13.p.rapidapi.com/city/latlon/${lat}/${lon}`;
+    // getWeatherInfo(url);
   }, function(error) {
     console.log("Error occurred. Error code: " + error.code);
   });
@@ -69,34 +70,34 @@ if (navigator.geolocation) {
 let getWeatherInfo = async (url) =>{
   
   const options = {
-	method: 'GET',
-	headers: {
-		'x-rapidapi-key': '5bae2b20b3mshfa5923da98afd77p138521jsnce7c2c61fc0e',
-		'x-rapidapi-host': 'weatherapi-com.p.rapidapi.com'
-	}
-};
-try {
-	const response = await fetch(url, options);
-	const result = await response.json();
-	console.log(result);
-  placeName.textContent = result.location.name+", "+result.location.region+", "+result.location.country;
-  longitude.textContent = result.location.lon;
-  latitude.textContent = result.location.lat;
-  temperature.textContent = result.current.temp_c;
-  desc.textContent = result.current.condition.text;
-  feelsliketemp.textContent = result.current.feelslike_c;
-  wind.textContent = result.current.wind_kph;
-  wdir.textContent = result.current.wind_dir;
-
-  pressure.textContent = result.current.pressure_mb;
-  humidity.textContent = result.current.humidity;
-  visibility.textContent = result.current.vis_km;
-  clouds.textContent = result.current.cloud;
-  let url2 = `https://weatherapi-com.p.rapidapi.com/forecast.json?q=${result.location.name}&days=3`;
-  getWeatherForecast(url2);
-} catch (error) {
-	console.error(error);
-}
+    method: 'GET',
+    headers: {
+      'x-rapidapi-key': '5bae2b20b3mshfa5923da98afd77p138521jsnce7c2c61fc0e',
+      'x-rapidapi-host': 'open-weather13.p.rapidapi.com'
+    }
+  };
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    console.log(result);
+    placeName.textContent = result.name+","+result.sys.country;
+    longitude.textContent = result.coord.lon;
+    latitude.textContent = result.coord.lat;
+    temperature.textContent = (result.main.temp-273).toFixed(2);
+    desc.textContent = result.weather[0].description;
+    feelsliketemp.textContent = (result.main.feels_like-273.15).toFixed(2);
+    wind.textContent = result.wind.speed;
+    // wdir.textContent = result.current.wind_dir;
+    pressure.textContent = result.main.pressure;
+    humidity.textContent = result.main.humidity;
+    visibility.textContent = result.visibility;
+    clouds.textContent = result.clouds.all;
+    let url2 = `https://weatherapi-com.p.rapidapi.com/forecast.json?q=${result.location.name}&days=3`;
+    getWeatherForecast(url2);
+  } 
+  catch (error) {
+	  console.error(error);
+  }
 };
 let getWeatherForecast= async(url)=>{
   const options = {
@@ -135,31 +136,40 @@ let result;
 searchBtn.addEventListener('click',()=>{
   const search = document.querySelector(".search");
   city = search.value;
-  let url = `https://weatherapi-com.p.rapidapi.com/current.json?q=${city}`;
-  getWeatherInfo(url);
+  let url = `https://open-weather13.p.rapidapi.com/city/${city}/EN`;
+  getWeatherInfo2(url);
 });
 
-// Thought of the day
-async function findThought() {
-  const urlThought = "https://thought-of-the-day.p.rapidapi.com/thought";
-  const optionsThought = {
-    method: "GET",
+let getWeatherInfo2 = async (url) =>{
+  
+  const options = {
+    method: 'GET',
     headers: {
-      "x-rapidapi-key": "5bae2b20b3mshfa5923da98afd77p138521jsnce7c2c61fc0e",
-      "x-rapidapi-host": "thought-of-the-day.p.rapidapi.com",
-    },
+      'x-rapidapi-key': '5bae2b20b3mshfa5923da98afd77p138521jsnce7c2c61fc0e',
+      'x-rapidapi-host': 'open-weather13.p.rapidapi.com'
+    }
   };
-
   try {
-    const thought = await fetch(urlThought, optionsThought);
-    const resultThought = await thought.json();
-    const quote = document.querySelector(".quote");
-    quote.textContent = resultThought.data;
-    console.log(resultThought.data);
-  } catch (error) {
-    console.error(error);
+    const response = await fetch(url, options);
+    const result = await response.json();
+    console.log(result);
+    placeName.textContent = result.name+","+result.sys.country;
+    longitude.textContent = result.coord.lon;
+    latitude.textContent = result.coord.lat;
+    temperature.textContent = (((result.main.temp-32)*5)/9).toFixed(2);
+    desc.textContent = result.weather[0].description;
+    feelsliketemp.textContent = (((result.main.feels_like-32)*5)/9).toFixed(2);
+    wind.textContent = result.wind.speed;
+    // wdir.textContent = result.current.wind_dir;
+    pressure.textContent = result.main.pressure;
+    humidity.textContent = result.main.humidity;
+    visibility.textContent = result.visibility;
+    clouds.textContent = result.clouds.all;
+    let url2 = `https://weatherapi-com.p.rapidapi.com/forecast.json?q=${result.location.name}&days=3`;
+    getWeatherForecast(url2);
+  } 
+  catch (error) {
+	  console.error(error);
   }
-}
-findThought();
-
-
+};
+// Thought of the day
